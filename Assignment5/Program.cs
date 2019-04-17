e43801259aa8e5ad0bd9eed486485de672da6227
visualstudio <visualstudio@122-PC38>
2019-04-17 09:50:33 -0400

    deep

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -11,33 +11,52 @@ namespace Assignment5
     {
         static void Main(string[] args)
         {
-            { new GenerateList().Run(57); }
+            {
+                GenerateList a = new GenerateList();
+                a.Run(57);
+            }
         }
-
         class GenerateList
         {
             const int LEN = 100;
+
             ArrayList ListQ;
-            public void Run(int AddUpToNumber)
+
+            public void Run(int AddUptoNumber)
             {
                 Random r = new Random();
 
                 ListQ = new ArrayList();
-                for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
-                this.findTwoNumbersThatAddUpTo(AddUpToNumber);
-            }
-            public void findTwoNumbersThatAddUpTo(int AddUptoNumber)
-        {
-            Console.ReadLine();
-                }
-            
-            
 
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                    this.findTwoNumbersThatAddUpTo(AddUptoNumber);
+                    break;
+                }
             }
-  
-               
+            public void findTwoNumbersThatAddUpTo(int a)
+            {
+                Random r = new Random();
+
+                ListQ = new ArrayList();
+                int n1 = 0;
+                int n2 = 0;
+                for (int i = 0; i < LEN; i++)
+                {
+                    int num1 = ListQ.Add(r.Next(100));
+                    int num2 = ListQ.Add(r.Next(100));
+                    if (num1 + num2 == a)
+                    {
+                        n1 = num1;
+                        n2 = num2;
+                        Console.WriteLine($"The two numbers are {n1} and {n2}.");
+                    }
+                }
             }
         }
-    
+    }
+}
+
 
     
