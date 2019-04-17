70c66cceb1cef8a14c73c0ccbd86f08405c47b14
visualstudio <visualstudio@122-PC38>
2019-04-17 10:40:36 -0400

    Longestword

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -3,60 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
 
-namespace Assignment5
+namespace Assignment_5_Part_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            {
-                GenerateList a = new GenerateList();
-                a.Run(57);
-            }
+            Program a = new Program();
+            Console.Write("The Longest word is : ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
         }
-        class GenerateList
+
+        public string MyFunctionA(string input)
         {
-            const int LEN = 100;
-
-            ArrayList ListQ;
-
-            public void Run(int AddUptoNumber)
+            string[] words = input.Split(' ');
+            int wordArrayLength = words.Length;
+            int[] wordsLength = new int[wordArrayLength];
+            int x = 0;
+            foreach (var word in words)
             {
-                Random r = new Random();
-
-                ListQ = new ArrayList();
-
-                for (int i = 0; i < LEN; i++)
+                if (word.Length > x)
                 {
-                    ListQ.Add(r.Next(100));
-                    this.findTwoNumbersThatAddUpTo(AddUptoNumber);
-                    break;
+                    input = word;
+                    x = word.Length;
                 }
             }
-            public void findTwoNumbersThatAddUpTo(int a)
-            {
-                Random r = new Random();
+            string currentWord = words[0];
+            string nextWord;
+            string longestWord = currentWord;
 
-                ListQ = new ArrayList();
-                int n1 = 0;
-                int n2 = 0;
-                for (int i = 0; i < LEN; i++)
-                {
-                    int num1 = ListQ.Add(r.Next(100));
-                    int num2 = ListQ.Add(r.Next(100));
-                    if (num1 + num2 == a)
-                    {
-                        n1 = num1;
-                        n2 = num2;
-                        Console.WriteLine($"The two numbers are {n1} and {n2}.");
-                    }
-                }
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
             }
+            return input;
         }
     }
 }
 
 
-    
+
