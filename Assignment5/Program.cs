5cd5d4aa02f1a5c5d0059a17f329876599a78b91
visualstudio <visualstudio@122-PC38>
2019-04-17 09:07:09 -0400

    deep

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Assignment5
 {
@@ -10,6 +11,33 @@ namespace Assignment5
     {
         static void Main(string[] args)
         {
+            { new GenerateList().Run(57); }
         }
-    }
-}
+
+        class GenerateList
+        {
+            const int LEN = 100;
+            ArrayList ListQ;
+            public void Run(int AddUpToNumber)
+            {
+                Random r = new Random();
+
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
+                this.findTwoNumbersThatAddUpTo(AddUpToNumber);
+            }
+            public void findTwoNumbersThatAddUpTo(int AddUptoNumber)
+        {
+            Console.ReadLine();
+                }
+            
+            
+
+            }
+  
+               
+            }
+        }
+    
+
+    
